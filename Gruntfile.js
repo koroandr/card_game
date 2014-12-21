a2ea638b873de4b80f0fcb862f85a22018a5b563
koroandr <koroandr@gmail.com>
2014-12-21 03:41:30 +0300

    Init commit

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+var grunt = require('grunt');
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jasmine: {
+        src : ['src/**/*.js'],
+        options : {
+            specs : 'test/**/*.js'
+        }
+    },
+    ts: {
+        default : {
+            src: ["src/**/*.ts", "test/**/*.ts", "!node_modules/**/*.ts"],
+            options: {
+                module: "amd"
+            }
+        }
+    },
+    clean: ["src/**/*.t.js", "src/**/*.i.js", "src/**/*.js.map", "test/**/*.t.js", "test/**/*.js.map"]
+});
+
+grunt.loadNpmTasks("grunt-ts");
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+grunt.registerTask("default", ["ts"]);
+
+grunt.registerTask('test', ['jasmine']);
---
+++ test/model/test.jasmine.t.ts
@@ -0,0 +1,6 @@
+/// <reference path="../../DefinitelyTyped/jasmine/jasmine.d.ts" />
+describe('base_test', ()=>{
+    it('ololo', ()=>{
+        expect(true).toBe(true);
+    });
+});
